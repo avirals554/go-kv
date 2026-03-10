5630b51a91f041d67ad5e61859cae571e1017078
Aviral <avirals554@Avirals-MacBook-Air.local>
2026-03-10 13:30:57 +0530

    added concurrency through go routines

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 	"strings"
 )
 
+var store = make(map[string]string)
+
 func main() {
-	store := make(map[string]string)
 	listen, err := net.Listen("tcp", ":5379")
 	if err != nil {
 		fmt.Println("didnt find any connection sorry ")
@@ -19,30 +20,33 @@ func main() {
 			fmt.Println("couldnt connect sorry ")
 			return
 		}
-		fmt.Println("we are getting a connection from  ", conn.RemoteAddr())
-		conn.Write([]byte("tcp connection has started ......."))
-		for {
-			buf := make([]byte, 1024)
-			n, err := conn.Read(buf)
-			if err != nil {
-				fmt.Println("something went went wrong line 26 onwards ")
+		go makeconnection(conn)
+	}
+}
+func makeconnection(conn net.Conn) {
+	fmt.Println("we are getting a connection from  ", conn.RemoteAddr())
+	conn.Write([]byte("tcp connection has started ......."))
+
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("something went went wrong line 26 onwards ")
+			break
+		}
+		message := strings.Split(string(buf[:n]), " ")
+		if message[0] == "GET" {
+			value, ok := store[message[1]]
+			if !ok {
+				fmt.Println("couldnt find ")
 				break
 			}
-			message := strings.Split(string(buf[:n]), " ")
-			if message[0] == "GET" {
-				value, ok := store[message[1]]
-				if !ok {
-					fmt.Println("couldnt find ")
-					break
-				}
-				conn.Write([]byte(value))
-			}
-			if message[0] == "SET" {
-				store[message[1]] = message[2]
-
-			}
+			conn.Write([]byte(value))
 		}
+		if message[0] == "SET" {
+			store[message[1]] = message[2]
 
+		}
 	}
 
 }
