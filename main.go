64b5df6cb1846ba89fc3900316b40ea49623d4bd
Aviral <avirals554@Avirals-MacBook-Air.local>
2026-03-11 15:08:49 +0530

    added persistence

--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 	"strings"
 )
 
 var store = make(map[string]string)
+var db, err = os.OpenFile("val.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
 
-func main() {
-	listen, err := net.Listen("tcp", ":5379")
-	if err != nil {
-		fmt.Println("didnt find any connection sorry ")
-		return
-	}
-	for {
-		conn, err := listen.Accept()
-		if err != nil {
-			fmt.Println("couldnt connect sorry ")
-			return
+func load_from_disk() {
+	scanner := bufio.NewScanner(db)
+	for scanner.Scan() {
+		line := scanner.Text()
+		command := strings.Split(line, " ")
+		if command[0] == "SET" {
+			store[command[1]] = command[2]
+
 		}
-		go makeconnection(conn)
 	}
 }
 func makeconnection(conn net.Conn) {
@@ -31,7 +30,7 @@ func makeconnection(conn net.Conn) {
 		buf := make([]byte, 1024)
 		n, err := conn.Read(buf)
 		if err != nil {
-			fmt.Println("something went went wrong line 26 onwards ")
+			fmt.Println("something went went wrong conn.read()")
 			break
 		}
 		message := strings.Split(string(buf[:n]), " ")
@@ -45,8 +44,28 @@ func makeconnection(conn net.Conn) {
 		}
 		if message[0] == "SET" {
 			store[message[1]] = message[2]
+			saving_message := strings.Join(message, " ")
+
+			db.Write([]byte(saving_message + "\n"))
 
 		}
 	}
 
+}
+func main() {
+
+	load_from_disk()
+	listen, err := net.Listen("tcp", ":5379")
+	if err != nil {
+		fmt.Println("didnt find any connection sorry ")
+		return
+	}
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("couldnt connect sorry ")
+			return
+		}
+		go makeconnection(conn)
+	}
 }
