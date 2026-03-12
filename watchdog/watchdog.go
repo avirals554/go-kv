0ed020501a8e1aac896c04f8f527176dafe2203e
Aviral <avirals554@Avirals-MacBook-Air.local>
2026-03-12 14:35:01 +0530

    finishing the watchdog and adding the proxy

--- watchdog/watchdog.go
+++ watchdog/watchdog.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"time"
 )
 
+var message_to_proxy, err = os.OpenFile("active_node.txt", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+
 func main() {
 	count := 0
-
+	message := "5379"
+	message_to_proxy.Truncate(0)
+	message_to_proxy.Seek(0, 0)
+	message_to_proxy.Write([]byte(message + "\n"))
 	for {
+
 		conn, err := net.Dial("tcp", "localhost:5379")
 		if err != nil {
 			count++
@@ -17,9 +24,16 @@ func main() {
 		if err == nil {
 			conn.Close()
 			count = 0
+
 		}
 		if count == 3 {
 			fmt.Println("the leader has failed ....")
+			if message != "5380" {
+				message = "5380"
+				message_to_proxy.Truncate(0)
+				message_to_proxy.Seek(0, 0)
+				message_to_proxy.Write([]byte(message + "\n"))
+			}
 		}
 		time.Sleep(3 * time.Second)
 
